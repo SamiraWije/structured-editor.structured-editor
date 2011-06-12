83ef210a9ecbb30558f78dd3e5220611bd682d18
oleg <oleg@localhost>
2011-06-12 16:20:34 +0400

    Structured-editor repository created

---
+++ src/ru/ipo/structurededitor/view/events/RepaintEvent.java
@@ -0,0 +1,14 @@
+package ru.ipo.structurededitor.view.events;
+
+import java.util.EventObject;
+
+/**
+ * Event: editor repaint
+ */
+public class RepaintEvent extends EventObject {
+
+    public RepaintEvent(Object source) {
+        super(source);
+    }
+
+}
